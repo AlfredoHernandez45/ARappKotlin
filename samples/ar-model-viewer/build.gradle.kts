a4a2068a9573a1b8a958d0c644a7ec3813eaea03
AlfredoHernandez45 <jose16alfredo04@gmail.com>
2026-01-29 21:11:58 -0500

    Agregando mapa en aplicacion y Firmando Aplicacion

--- samples/ar-model-viewer/build.gradle.kts
+++ samples/ar-model-viewer/build.gradle.kts
@@ -16,6 +16,26 @@ android {
         versionName = "1.0.0"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release-key.jks")
+            storePassword = "jose123"
+            keyAlias = "my-key-alias"
+            keyPassword = "jose123"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
         targetCompatibility = JavaVersion.VERSION_21
